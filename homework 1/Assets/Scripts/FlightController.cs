f618c1ab833ae8a801ec5b14e09db4cb2c23cada
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-03-17 21:06:59 +0300

    Time.deltaTime for rotational values

--- homework 1/Assets/Scripts/FlightController.cs
+++ homework 1/Assets/Scripts/FlightController.cs
@@ -31,9 +31,9 @@ public class FlightController : MonoBehaviour
         // TODO (Task 3-C): 
         // Pitch   
         // Roll    
-        float roll = Input.GetAxis("Roll") * rollSpeed; // roll left/right with A/D or Left/Right Arrow keys
-        float pitch = Input.GetAxis("Pitch") * pitchSpeed; // pitch up/down with W/S or Up/Down Arrow keys
-        float yaw = Input.GetAxis("Yaw") * yawSpeed; // yaw left/right with mouse movement
+        float roll = Input.GetAxis("Roll") * rollSpeed * Time.deltaTime; // roll left/right with A/D or Left/Right Arrow keys
+        float pitch = Input.GetAxis("Pitch") * pitchSpeed * Time.deltaTime; // pitch up/down with W/S or Up/Down Arrow keys
+        float yaw = Input.GetAxis("Yaw") * yawSpeed * Time.deltaTime; // yaw left/right with mouse movement
 
         transform.Rotate(pitch, roll, yaw, Space.Self);
     } 
